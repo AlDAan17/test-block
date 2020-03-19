ede343e33f3163507570a407019d447a1493c87b
Aldani <kototi17@gmail.com>
2020-03-19 18:22:11 +0300

    ararage

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function readMore(){
+    let btn = document.getElementsByClassName("content__about__more");
+    let dots = document.getElementById("dots");
+    let dots = document.getElementById("dots");
+
+}
